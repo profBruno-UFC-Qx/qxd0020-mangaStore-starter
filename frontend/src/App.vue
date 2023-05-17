c02d78c284fe6cef270fcd731df91ccc1f415038
Bruno Mateus <brunomateus@gmail.com>
2023-05-17 15:54:18 -0300

    Código da segunda aula

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { api } from './baseConfig'
+import MangaCard from './components/MangaCard.vue'
+
+const mangas = ref()
+const response = await api.get('/mangas')
+mangas.value = response.data.data; 
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col" v-for="manga in mangas">
+          <MangaCard :title="manga.title" :price="manga.price" :number="manga.number"></MangaCard> 
+        </div>
+
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -21,9 +29,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ frontend/src/components/MangaCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+  number: number,
+  price: number
+}>()
+</script>
+
+<template>
+  <div class="card shadow-sm">
+    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img"
+      aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+        dy=".3em">Thumbnail</text>
+    </svg>
+    <div class="card-body">
+      <h3 class="card-title">{{ title }}</h3>
+      <p class="card-text">Volume: {{ number }}</p>
+      <p class="card-text">Price: {{ price }}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+        </div>
+        <small class="text-body-secondary">9 mins</small>
+      </div>
+    </div>
+  </div>
+</template>
